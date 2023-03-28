2f68de1dc8eb209960ddeedba129a03f0a96cb61
DEV-DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-03-27 21:05:14 -0400

    chore: some more slight changes

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ Website.get('/', async (Request, Response) => {
     await Response.status(200).sendFile('./Pages/index.html', { root: __dirname });
 });
 
-Website.get('/contact', async (Request, Response) => {
-    await Response.status(200).sendFile('./Pages/contact.html', { root: __dirname });
-});
+// Website.get('/contact', async (Request, Response) => {
+//     await Response.status(200).sendFile('./Pages/contact.html', { root: __dirname });
+// });
 
 //Serving static Website Assets.
 Website.use('/Assets', ExpressJS.static('Assets'));
