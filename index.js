ff301e76948df7e670da4d6b2b96f6a749595eaa
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-11-16 21:05:34 -0500

    feat: attempting something new

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ExpressJS = require('express');
 const Chalk = require('chalk');
 const Config = require('./config.json');
+const Path = require('path');
 
 const Website = ExpressJS();
 Website.use(ExpressJS.static('Pages'));
@@ -11,6 +12,22 @@ Website.get('/', async (Request, Response) => {
 
 Website.get('/contact', async (Request, Response) => {
     await Response.status(200).sendFile('./Pages/contact.html', { root: __dirname });
+});
+
+//Dynamically serve website content.
+Website.get('/components/:filename', async (Request, Response) => {
+    const FileName = Request.params.filename;
+    const FilePath = Path.join(__dirname, 'Components', FileName);
+
+    await Response.status(200).sendFile(FilePath);
+});
+
+//Dynamically serve scripts.
+Website.get('/scripts/:filename', async (Request, Response) => {
+    const FileName = Request.params.filename;
+    const FilePath = Path.join(__dirname, 'Scripts', FileName);
+
+    await Response.status(200).sendFile(FilePath);
 });
 
 Website.get('/projects', async (Request, Response) => {
